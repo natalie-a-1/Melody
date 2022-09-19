569a07a898284a28af143e5d7bc0f9f695aa98a9
nataliehill <natalie.a.hill-1@ou.edu>
2022-09-19 15:04:20 -0500

    started User class

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SpotifyAccount account = new SpotifyAccount();
+        User Natalie = new User("Natalie", "temp", account);
+        System.out.println(Natalie.getName());
+    }
+}
