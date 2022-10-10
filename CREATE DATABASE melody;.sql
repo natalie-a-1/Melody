151065526d70a2b71d043e86fe1c696cdf96c9c8
nataliehill <natalie.a.hill-1@ou.edu>
2022-10-10 10:07:30 -0500

    command file for mySql

--- CREATE DATABASE melody;.sql
+++
@@ -1,9 +0,0 @@
--- CREATE DATABASE melody;
--- CREATE TABLE IF NOT EXISTS melody.users(
---     id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     userName VARCHAR(200) NOT NULL,
---     password VARCHAR(200) NOT NULL
--- );
-
-INSERT INTO melody.users(userName, password)
-VALUES ('Preston', 'password2');
